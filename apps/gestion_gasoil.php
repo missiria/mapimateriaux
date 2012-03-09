f164ba23104e4b15d9d8a9e98026ff8adc5ca629
MISSIRIA <missiria@gmail.com>
2012-03-09 17:42:38 +0000

    SEARCH & DELETE (DONE)

--- apps/gestion_gasoil.php
+++ apps/gestion_gasoil.php
@@ -55,35 +55,121 @@
                   $db->query($sSQL);
                   echo '<script>alert("Vous avez saisie le bon numéro : '. $num_bon .'")</script>';
             }
-      $keyword = get_param("keyword");
       
-      if (!$keyword) {
-            //echo '<script>alert("il faut saisie qq chose pour cherche !");</script>';
-            //echo '<script>$("table").addClass("hide");</script>';
-            //$tpl->pparse("block_Search", false);      
-      }
-      
-      if ($keyword){
-            $tpl->set_var("keyword", $keyword);
-            $sql = "SELECT * 
-                    FROM  suivi_gasoil 
-                    WHERE  num_bon LIKE " . tosql("%".trim($keyword) ."%", "Text");
+      search();
+      //==============================
+      function search() {
+      //==============================
+            global $db;
+            global $tpl;
+            global $sForm;
+            $sActionFileName = "gestion_gasoil.php";
+            $tpl->set_var("ActionPage", $sActionFileName);
+            $keyword = strip(get_param("keyword"));
+            $tpl->set_var("keyword", tohtml($keyword));
             
-            db_fill_array($sql);
-            $Ssql = $db->query($sql);
-            if(is_array($Ssql)) {
-                  reset($Ssql);
-                  while(list($value, $key) = each($Ssql)) {
-                    $fldnum_bon = $db->f("s_num_bon");
-                    $fldid = $db->f("s_id");
-                    $tpl->set_var("Value", $fldnum_bon);
-                    $tpl->set_var("id", $fldid);
-                    $tpl->parse("block_search", false);
-                  }
+            if ($keyword == "") {
+                  $query = mysql_query("SELECT *
+                                        FROM suivi_gasoil");
+                                        
+             while($row = mysql_fetch_array( $query )) 
+             { 
+             //WE DISPLAY THE RESULTS
+             $id = $row['id'];
+             $date = $row['date']; 
+             $km = $row['km']; 
+             $qt_littre = $row['qt_littre'];
+             $num_bon = $row['num_bon']; 
+             $ref_vehicule = $row['ref_vehicule']; 
+             
+             // VARIABLE WE SET INTO TEMPLATE
+             $tpl->set_var("id", $id);
+             $tpl->set_var("date", $date);
+             $tpl->set_var("km", $km);
+             $tpl->set_var("qt_littre", $qt_littre);
+             $tpl->set_var("num_bon", $num_bon);
+             $tpl->set_var("ref_vehicule", $ref_vehicule);
+             $tpl->parse("row_result", true); 
+             }
             }
-
+            
+            $query = mysql_query("SELECT *
+                                  FROM suivi_gasoil 
+                                  WHERE num_bon 
+                                  LIKE ". $keyword ."");
+            while($row = mysql_fetch_array( $query )) 
+             { 
+             //WE DISPLAY THE RESULTS
+             $id = $row['id'];
+             $date = $row['date']; 
+             $km = $row['km']; 
+             $qt_littre = $row['qt_littre'];
+             $num_bon = $row['num_bon']; 
+             $ref_vehicule = $row['ref_vehicule']; 
+             
+             // VARIABLE WE SET INTO TEMPLATE
+             $tpl->set_var("id", $id);
+             $tpl->set_var("date", $date);
+             $tpl->set_var("km", $km);
+             $tpl->set_var("qt_littre", $qt_littre);
+             $tpl->set_var("num_bon", $num_bon);
+             $tpl->set_var("ref_vehicule", $ref_vehicule);
+             $tpl->parse("row_result", true); 
+             } 
+            
+            /* if ($keyword) {
+                  
+            $lookup_num_bon = db_fill_array("SELECT date, num_bon, ref_vehicule, qt_littre, km
+                                             FROM suivi_gasoil 
+                                             WHERE num_bon LIKE ". $keyword ."");
+                  echo "test :"; print_r($lookup_num_bon);
+                  if(is_array($lookup_num_bon)) {
+                        reset($lookup_num_bon);
+                        
+                        while(list($key, $value) = each($lookup_num_bon)) {
+                        
+                              if ($value == $keyword) {
+	                              $tpl->set_var("ID", $key);
+                                    $tpl->set_var("NUM_BON", $value);
+                                    $tpl->set_var("DATE", $value);
+                                    
+	                        } else {
+	                              $tpl->set_var("DATE", "");
+                                    $tpl->set_var("NUM_BON", "");
+                                    $tpl->set_var("DATE", "");
+	                        }
+                           
+                           $tpl->parse("row_result", true);   
+	                        
+                        }
+                  }
+            } */
+            
+            $tpl->parse("block_search", false);
       }
-      
+      edit();
+      function edit() {
+	      $edit = get_param('edit');
+	      if ($edit) {
+	            $query = mysql_query("SELECT *
+                                  FROM suivi_gasoil 
+                                  WHERE num_bon 
+                                  LIKE ". $keyword ."");
+                        
+	      }
+	  //$tpl->parse("block_edit", false);
+      }
+      delete();
+      function delete() {
+	      $delete = get_param('delete');
+	      if ($delete) {
+	            $sql = sprintf("DELETE FROM suivi_gasoil WHERE id = '".$delete."'");
+	            $result = mysql_query($sql);
+	            
+	      }
+	
+      }
+   
    $tpl->pparse("gestion_gasoil", false);      
 
 ?>
